d3a88ca9835380f87aa7e560d466898135b54655
Mariano Fajardo Islas <mfajardoi.prac@xcaret.com>
2024-05-27 16:43:22 -0500

    Nuevos cambios con custom

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Counter from './components/Counter';
 import { Usuario } from './components/Usuario';
 
 import { TimerPadre } from './components/TimerPadre';
+import { ContadorRed } from './components/ContadorRed';
 
 function App() {
   return (
@@ -16,6 +17,10 @@ function App() {
       <hr />
       
       <TimerPadre/>
+
+      <h2>UseReducer</h2>
+      <hr />
+      <ContadorRed/>
 
     </div>
   );
