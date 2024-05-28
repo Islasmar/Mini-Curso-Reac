f8bc673204e48f2eb5274ce7dafb24687e794d25
Islasmar <marianoislasfajardo@gmail.com>
2024-05-27 21:09:09 -0600

    Continuación del fomrulario.

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,19 +1,52 @@
+import { ChangeEvent, useState } from "react";
+import { useForm } from "../hooks/useForm";
+
 export const Formulario = () => {
+
+  const {formulario,handleChange} = useForm({
+    email: 'marianoislas@gmail.com',
+    nombre: 'Mariano Islas'
+  });
+
+  // const [formulario, setFormulario] = useState(
+  //   {
+  //     email: '',
+  //     nombre: ''
+  //   }
+  // );
+
+  // const handieChange = ({target}: ChangeEvent <HTMLInputElement>) =>{
+  //   const{name, value} =target;
+
+  //     setFormulario({
+  //       ...formulario,
+  //       [name]: value
+  //     })
+  
+
   return (
     <form autoComplete="off">
+
         <div className="mb-3">
             <label className="form-label">Email:</label>
             <input type="email"
             className="form-control"
-            name="email"/>
+            name="email"
+            value={formulario.email}
+            onChange={handleChange}/>
         </div>
 
         <div className="mb-3">
             <label className="form-label">Nombre:</label>
-            <input type="email"
+            <input type="text"
             className="form-control"
-            name="email"/>
+            name="nombre"
+            value={formulario.nombre}
+            onChange={handleChange}/>
         </div>
+
+        <pre>{JSON.stringify(formulario)}</pre>
+
     </form>
   )
 }
